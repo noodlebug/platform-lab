ee1d7644c5161fefdbb1094337e6a802d570be72
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2018-02-06 17:37:23 +0000

    First Commit - empty project

---
+++ platform-lab-project/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//  in game player input and control of the player entity
+
+public class PlayerController : MonoBehaviour
+{
+}
