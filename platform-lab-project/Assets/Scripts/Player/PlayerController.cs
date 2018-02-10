0081058157c8267fb40f18f6c267873c9bbb8701
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2018-02-10 13:46:23 +0000

    Moved curent player movement code to SimulatedPhysics. PlayerController can have different states now.

--- platform-lab-project/Assets/Scripts/Player/PlayerController.cs
+++ platform-lab-project/Assets/Scripts/Player/PlayerController.cs
@@ -3,30 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //  in game player input and control of the player entity
+public enum PlayerType {Simulated}; 
 
 public class PlayerController : MonoBehaviour
 {
     //  private
-    private Rigidbody2D rigidBody;
-    private bool move;
-    private bool jump = false;
-    private bool grounded = false;
-    private SpriteRenderer spriteRenderer;
+    [HideInInspector]public GameManager game;    
+    [HideInInspector]public Rigidbody2D rigidBody;
+    [HideInInspector]public SpriteRenderer spriteRenderer;
     private List<Interactable> interactables = new List<Interactable>();
+    private SimulatedPhysics simulatedPhysics;
 
-    //  public
-    public GameManager game;
+    public PlayerType playerType;
+
+    [Header("SimulatedPhysics")]
     public Transform belowPlayer;
-    public float acceleration = 300f;
-    public float maxSpeed = 5f;
-    public float jumpForce = 250f;
-    public float airbourneAcceleration = 150f;
+
+    [Header("General physics modifiers")]
+    //  delta values applied to all movement scripts
+    public float acceleration = 1f;
+    public float speed = 1f;
+    public float jump = 1f;
+    public float airbourneAcceleration = 1f;
+
+
 
     //  when player spawns
     private void Awake()
     {
         rigidBody= GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        simulatedPhysics = new SimulatedPhysics(this, belowPlayer, 300f, 2f, 350f, 50f);
     }
     
     //  //
@@ -65,72 +73,15 @@ public class PlayerController : MonoBehaviour
             //  //
     private void Update()
     {
-        game.debug.Log("Position X:", System.Math.Round(transform.position.x, 2).ToString());//DEBUG
-
-        CheckJump();
-
-        CheckFacing();
-
-        InteractUpdateInput();
-
-        //  fix rotation to prevent obeject from falling over
-        transform.rotation = new Quaternion();
+        if (playerType == PlayerType.Simulated)
+        {
+            simulatedPhysics._Update();
+        }
+        InteractUpdateInput();            
     }
 
     //  can player jump and is jump pressed
-    private void CheckJump()
-    {
-        //  plot a line between either side of the player, just below the player
-        float offset = spriteRenderer.bounds.size.x / 2;
-        Vector2 leftBelow = new Vector2(belowPlayer.position.x - offset, belowPlayer.position.y);
-        Vector2 rightBelow = new Vector2(belowPlayer.position.x + offset, belowPlayer.position.y);
-
-        //  raycast to check if player is grounded
-        grounded = Physics2D.Linecast(leftBelow, rightBelow, 1 << LayerMask.NameToLayer("Ground"));
-        game.debug.Log("Grounded: ", grounded.ToString());//DEBUG
-
-        //  on ground and jump pressed
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
-        {
-            jump = true;
-        }
-    }
-
-    //  direction key pressed
-    private void CheckFacing()
-    {
-        //  flip sprite when direction keys are pressed
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            FaceRight();
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            FaceLeft();
-        }
-    }
-
-    //turn left or right
-    private void FaceRight()
-    {
-        Turn(1);
-    }
-    private void FaceLeft()
-    {
-        Turn(-1);
-    }
-    private void Turn(int sign)
-    {
-        //  if on ground stop moving before changing movement direction
-        if (grounded)
-        {
-            rigidBody.velocity = Vector2.zero;
-        }
-        Vector2 scale = transform.localScale;
-        scale.x = sign;
-        transform.localScale = scale;
-    }
-
+   
     #endregion
 
     //  //
@@ -139,50 +90,10 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Jump();
-
-        Move(grounded);
-    }
-
-    //  add upward force
-    private void Jump()
-    {
-        if (grounded && jump)
+        if (playerType == PlayerType.Simulated)
         {
-            rigidBody.AddForce(new Vector2(0f, jumpForce));
-            jump = false;
+            simulatedPhysics._FixedUpdate();        
         }
-    }
-
-    //  add force relative to Input.GetAxis() and clamp speed to max
-    private void Move(bool grounded = true)
-    {
-        float hAxis = Input.GetAxis("Horizontal");
-        if (hAxis == 0)
-        {
-            return;            
-        }
-
-        //  if not exceeding max speed, accelerate
-        if (hAxis * rigidBody.velocity.x < maxSpeed)
-        {
-            //  airbourne acceleration
-            if (!grounded)
-            {
-                rigidBody.AddForce(Vector2.right * hAxis * airbourneAcceleration);
-            }
-            //  normal acceleration
-            else
-            {
-                rigidBody.AddForce(Vector2.right * hAxis * acceleration);
-            }
-
-            //  clamp speed
-            if (Mathf.Abs(rigidBody.velocity.x) > maxSpeed)
-            {
-                rigidBody.velocity = new Vector2(Mathf.Sign(rigidBody.velocity.x) * maxSpeed, rigidBody.velocity.y);
-            }
-        } 
     }
 
     #endregion
