4246850ef03ed5fa986b9156532034e7ad42beae
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2018-02-11 19:43:56 +0000

    Added simple trapdoor

---
+++ platform-lab-project/Assets/Trapdoor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//	simple trapdoor
+
+public class Trapdoor : MonoBehaviour
+{
+	public GameObject leftPivot;
+	public GameObject rightPivot;
+
+	public bool open = false;
+	public void Toggle()
+	{
+		int adjust = open ? -90 : 90;
+		open = !open;
+
+		leftPivot.transform.Rotate(new Vector3(0, 0, -adjust));
+		rightPivot.transform.Rotate(new Vector3(0, 0, adjust));
+	}
+}
